d83b6164c653844bcd26403e7879635dcc1ebfdf
mcsrk <49255542+mcsrk@users.noreply.github.com>
2023-01-07 00:14:38 -0500

    improved sales card

--- src/components/SaleCard.jsx
+++ src/components/SaleCard.jsx
@@ -1,12 +1,23 @@
 import * as React from "react";
+
+// Styles
 import "./SaleCard.scss";
-const SaleCard = ({ title, amount, date }) => {
+
+// Assets
+import iconInfo from "../assets/iconInfo.svg";
+
+// Utils
+import { formatColombianCurrency } from "../utils/utils";
+
+const SaleCard = ({ data }) => {
+  const { title, amount, date } = data;
   return (
     <div className="sale-card">
       <div className="card-header">
         <div className="header-title">{title}</div>
+        <img className="icon-info" src={iconInfo} alt="info-icon" />
       </div>
-      <span className="amount">{amount}</span>
+      <span className="amount">{formatColombianCurrency(amount)}</span>
       <span className="date">{date}</span>
     </div>
   );
