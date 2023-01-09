dc2ac62fc25449cae2a610533a862cfbb68a52f3
mcsrk <49255542+mcsrk@users.noreply.github.com>
2023-01-09 17:23:17 -0500

    format and some quic styles

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,13 +1,15 @@
 import * as React from "react";
+
+// Styles
 import "./Filter.scss";
+
 import iconFilter from "../assets/iconFilter.svg";
+
 const FilterButton = (props) => {
   return (
     <button className="filter-button">
-      {/* <div className="content-6"> */}
       <span className="text">{props.text || "FILTRAR"}</span>
       <img className="icon-filter" src={iconFilter} alt="filter-icon" />
-      {/* </div> */}
     </button>
   );
 };
